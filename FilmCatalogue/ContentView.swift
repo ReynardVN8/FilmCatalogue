fdd98757f0ba7e208915b2573579384bab3a6b86
ReynardVN8 <reynardvn8@gmail.com>
2022-10-10 17:32:58 +0700

    Initial Commit

---
+++ FilmCatalogue/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FilmCatalogue
+//
+//  Created by Reynard Vincent Nata on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FilmCatalogue/FilmCatalogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmCatalogueApp.swift
+//  FilmCatalogue
+//
+//  Created by Reynard Vincent Nata on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FilmCatalogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
