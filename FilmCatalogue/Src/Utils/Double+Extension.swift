8c3ccb866d377c7831e8f51f66bed81291b7d190
ReynardVN8 <reynardvn8@gmail.com>
2022-11-07 16:53:46 +0700

    Add truncate functions

--- FilmCatalogue/Src/Utils/Double+Extension.swift
+++ FilmCatalogue/Src/Utils/Double+Extension.swift
@@ -11,4 +11,8 @@ extension Double {
     func toString1Decimal() -> String{
         return String(format: "%.1f", self)
     }
+    
+    func truncate(places : Int)-> Double {
+        return Double(floor(pow(10.0, Double(places)) * self)/pow(10.0, Double(places)))
+    }
 }
